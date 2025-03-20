dc82d901c28aa6855bcd9c485f35acfc473824f3
Nathan Nunes <nathanpedronunes@hotmail.com>
2025-03-20 14:40:00 -0300

    feat: enhance upload widget with file management and storage integration

---
+++ src/store/uploads.ts
@@ -0,0 +1,52 @@
+import { create } from "zustand";
+import { enableMapSet } from "immer";
+import { immer } from "zustand/middleware/immer";
+import { uploadFileToStorage } from "../http/upload-file-to-storage";
+
+export type Upload = {
+  name: string;
+  file: File;
+};
+
+type UploadState = {
+  uploads: Map<string, Upload>;
+  addUpload: (files: File[]) => void;
+};
+
+enableMapSet();
+
+export const useUploads = create<UploadState, [["zustand/immer", never]]>(
+  immer((set, get) => {
+    async function processUpload(uploadId: string) {
+      const upload = get().uploads.get(uploadId);
+
+      if (!upload) {
+        return;
+      }
+
+      await uploadFileToStorage({ file: upload.file });
+    }
+
+    function addUpload(files: File[]) {
+      for (const file of files) {
+        const uploadId = crypto.randomUUID();
+
+        const upload = {
+          name: file.name,
+          file,
+        };
+
+        set((state) => {
+          state.uploads.set(uploadId, upload);
+        });
+
+        processUpload(uploadId);
+      }
+    }
+
+    return {
+      uploads: new Map(),
+      addUpload,
+    };
+  })
+);
